3e783caf739bae3bd36564f1631bb9a66fc3c4d9
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-09-19 08:47:09 -0400

    resolve Lower/Uppercase issue

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import SelectingContainer from "./components/SelectingContainer";
 import ResultsContainer from "./components/ResultsContainer";
 
-
 class App extends Component {
   state = {
     posts: [],
@@ -16,36 +15,37 @@ class App extends Component {
 
     fetch(request)
       .then((res) => res.json())
-      .then((data) => this.setState({ posts: data.results, filteredposts: data.results }));
-
+      .then((data) =>
+        this.setState({ posts: data.results, filteredposts: data.results })
+      );
   }
 
   textsearched = (value) => {
-    let posts =[];
-    for(let i in this.state.posts){
-      let match=false;
-      let post=this.state.posts[i]
-      for(let prop in post){
-        if(post[prop].includes(value)){
-          match=true;
+    let posts = [];
+    for (let i in this.state.posts) {
+      let match = false;
+      let post = this.state.posts[i];
+
+      for (let prop in post) {
+        let lower = JSON.stringify(post[prop]).toLowerCase();
+        if (lower.includes(value.toLowerCase())) {
+          match = true;
         }
       }
-      if(match == true) {
-        posts.push(post)
+      if (match == true) {
+        posts.push(post);
       }
     }
-    this.setState({filteredposts:posts})
-    ; 
-  }
-
+    this.setState({ filteredposts: posts });
+  };
 
   render() {
     return (
       <div className="container2">
         <Header />
         <img src="http://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG32.png"></img>
-        <SelectingContainer 
-        textsearched = { (value) => this.textsearched(value)}
+        <SelectingContainer
+          textsearched={(value) => this.textsearched(value)}
         />
         <ResultsContainer posts={this.state.filteredposts} />
       </div>
--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,42 +1,18 @@
 import React, { Component } from "react";
 import "./Inputs.css";
-import json from "./data/data.json";
+
 
 class Inputs extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { suggestions: [] };
-    this.search = this.search.bind(this);
-   
-  }
-
-  search(event) {
-    let input = event.target.value;
-    let matches = [],
-      i;
-
-    if (input.length > 1) {
-      for (i = 0; i < json.length; i++) {
-        if (json[i].match(input)) {
-          matches.push(json[i]);
-        }
-      }
-    }
-    this.setState({ suggestions: matches });
-  }
-
-  
 
   render() {
-
     return (
       <div>
         {/* <form className="create-form"> */}
           <div className="form-group styling">
             <label>Search: </label>
             <input
+            id="inputs"
             onChange={ (event) => this.setState({searchtext: event.target.value})}
-              onKeyUp={this.search.bind(this)}
               type="text"
               style={{
                 color: "white",
@@ -48,7 +24,7 @@ class Inputs extends Component {
               type="submit" onClick={() => this.props.textsearched(this.state.searchtext)}
               style={{ color: "white", backgroundColor: "rgba(0,0,0,.2)" }}
             >
-              Find!
+              Filter!
             </button>
           </div>
         {/* </form> */}
